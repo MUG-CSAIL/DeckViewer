d2ceefdcad1e0b82c8630de02dd75d4b96460011
Kojo Acquah <kooolj@gmail.com>
2015-02-25 13:03:18 -0500

    fixed cataputl move to bug

--- src/edu/mit/kacquah/deckviewer/action/exec/QueueCatapultAction.java
+++ src/edu/mit/kacquah/deckviewer/action/exec/QueueCatapultAction.java
@@ -143,14 +143,19 @@ public class QueueCatapultAction extends SpeechGraph implements ExecAction {
       }
       
       // Move aircraft to their destinations
-//      int catIndex = 0;
-//      for (int i = 0; i < moveAircraft.size(); ++i) {
-//        FlyingObject o = moveAircraft.get(i);
-//        ParkingSpot p = catapultTargets.get(catIndex).getNextFreeParkingSpot();
-//        p.park(o);
-//        catIndex += 1;
-//        catIndex %= catapultTargets.size();
-//      }
+      int catIndex = 0;
+      for (int i = 0; i < moveAircraft.size(); ++i) {
+        FlyingObject o = moveAircraft.get(i);
+        ParkingSpot spot = catapultTargets.get(catIndex).getNextFreeParkingSpot();
+        if (spot == null) {
+          catIndex += 1;
+          catIndex %= catapultTargets.size();
+          spot = catapultTargets.get(catIndex).getNextFreeParkingSpot();
+        }        
+        spot.park(o);
+        catIndex += 1;
+        catIndex %= catapultTargets.size();
+      }
       // Move aircraft to their destinations
       for (int i = 0; i < moveAircraft.size(); ++i) {
         FlyingObject o = moveAircraft.get(i);
